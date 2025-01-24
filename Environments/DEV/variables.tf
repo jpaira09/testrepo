6cf49c3d49311d3ba7bf458a6eb88d7f09615b4d
Jagannath Paira <jagannath.paira@tavant.com>
2025-01-24 17:04:53 +0530

    add

--- Environments/DEV/variables.tf
+++ Environments/DEV/variables.tf
@@ -18,6 +18,6 @@ variable "region" {
   type = string
 }
 
-variable "VPC_NAME" {
+variable "vpc_name" {
   type = string
 }
--- Environments/DEV/vpc.tf
+++ Environments/DEV/vpc.tf
@@ -1,6 +1,6 @@
 module "prism_mw_vpc" {
   source = "../../Module/vpc" 
-  vpc_name = var.VPC_NAME
+  vpc_name = var.vpc_name
   vpc_map = {
     vpc_cidr          = "10.1.0.0/16"
     public_subnet_cidrs  = ["10.1.0.0/20", "10.1.16.0/20"]
